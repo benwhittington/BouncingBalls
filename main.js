2aadaa57e3ee3b18bd766e88ade6feddb32f1801
benwhittington <benjiwhittington@gmail.com>
2020-03-21 19:00:08 +1300

    add animation

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
-randomBetween = function(lower, upper) {
-    return Math.random()*(upper-lower)+lower;
-}
+class Colour {
+    constructor(r, g, b, a) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+    }
 
-function randomColour() {
-    return 'rgb(' + randomBetween(0,255) + ',' + randomBetween(0,255) + ',' + randomBetween(0,255) + ')';
+    getString() {
+        return 'rgba(' + this.r + ',' + this.g + ',' + this.b + ',' + this.a + ')';
+    }
 }
 
 class Ball {
-    constructor(ctx, color) {
-        this.r = 50;
-        this.x = randomBetween(this.r, window.innerWidth-this.r);
-        this.y = randomBetween(this.r, window.innerHeight-this.r);
+    constructor(ctx, colour) {
+        this.r = randomBetween(3, 50);
+        this.winX = window.innerWidth;
+        this.winY = window.innerHeight;
+        this.x = randomBetween(this.r, this.winX-this.r);
+        this.y = randomBetween(this.r, this.winY-this.r);
         this.vx = randomBetween(1, 10);
         this.vy = randomBetween(1, 10)
         this.ctx = ctx;
-        this.color = color;
+        this.colour = colour;
     }
 
     draw() {
-        this.ctx.fillStyle = this.color;
+        this.ctx.fillStyle = this.colour.getString();
         this.ctx.beginPath();
         this.ctx.arc(this.x,this.y,this.r,0,Math.PI*2,true);
         this.ctx.fill();        
@@ -28,12 +35,31 @@ class Ball {
         this.x += this.vx;
         this.y += this.vy;
     }
+
+    checkBoundaryCollision() {
+        if(this.x >= this.winX - this.r) {
+            this.vx *= -1;
+        }
+
+        if(this.x <= 0 + this.r) {
+            this.vx *= -1
+        }
+
+        if(this.y >= this.winY - this.r) {
+            this.vy *= -1;
+        }
+
+        if(this.y <= 0 + this.r) {
+            this.vy *= -1;
+        }
+    }
 }
 
 class ContextHandler {
     constructor(ctx) {
         this.ctx = ctx;
         this.balls = [];
+        this.backgroundColour = new Colour(0,0,0,1);
     }
 
     addBall(ball) {
@@ -44,8 +70,22 @@ class ContextHandler {
         for(let i = 0; i < this.balls.length; ++i) {
             this.balls[i].draw();
             this.balls[i].update();
+            this.balls[i].checkBoundaryCollision();
         }
     }
+
+    clear() {
+        this.ctx.fillStyle = this.backgroundColour.getString();
+        this.ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+    }
+}
+
+function randomColour() {
+    return new Colour(randomBetween(0,255), randomBetween(0,255), randomBetween(0,255), 1);
+}
+
+function randomBetween(lower, upper) {
+    return Math.random()*(upper-lower)+lower;
 }
 
 function inititaliseCanvas() {
@@ -55,29 +95,26 @@ function inititaliseCanvas() {
     return canvas.getContext('2d');
 }
 
-function doFrame(contextHandler) {
-    contextHandler.update();
-}
-
-function mainLoop(contextHandler) {
-    context.fillStyle = 'rgba(0,0,0.25)'
-}
-
 function main() {
     const context = inititaliseCanvas();
 
     let balls = [];
-    
     contextHandler = new ContextHandler(context);
+    let numberOfBalls = 5;
+    for(let i = 0;i < numberOfBalls; ++i) {
+        contextHandler.addBall(new Ball(context, randomColour()));
+    }
 
-    contextHandler.addBall(new Ball(context, 'rgb(255,0,0)'));
-    contextHandler.addBall(new Ball(context, 'rgb(0,255,0)'));
-    contextHandler.addBall(new Ball(context, randomColour()));
-    
-    
+    contextHandler.update();
 
+    function mainLoop() {
+        contextHandler.clear();
+        contextHandler.update();
+
+        requestAnimationFrame(mainLoop)
+    }
+
+    requestAnimationFrame(mainLoop);
 }
 
 main()
-
-
